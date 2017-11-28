092f0efe11be8a0edc67fed70ff9ce19e1abf754
Mardy <mardydiaz@ymail.com>
2017-11-27 22:30:09 -0600

    worked on file, added config .py

---
+++ config.py
@@ -0,0 +1,5 @@
+#Twitter API Keys
+consumer_key = '7Vvu5788znbSJ4yWCnajSA9Zt'
+consumer_secret = 'FgQlktG4w2XfzNuvpyk5YjZXi3S6wfBCxbZLdqzS8zMlPMiGqC'
+access_token = '930587998361673728-ZkoOx5zec1sXZpOcNYEXr8MXpa9BGWq'
+access_token_secret = 'JYdgIj9q85Z2bBHjKMZzxtWKGho25fqIEFhXmHZHuijBY'
